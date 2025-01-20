a628bf534c17f431334f41af7c9760a850946271
Rabifantasi <159634085+Rabifantasi@users.noreply.github.com>
2025-01-21 00:59:59 +0500

    changes

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
+  projectId: "8gl1jkrt",
+  dataset: "production",
+  apiVersion : "2022-03-07",
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
